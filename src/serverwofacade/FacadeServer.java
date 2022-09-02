49b06d611f6355e33624033e100b169a4f637112
tde514674 <tiniput_kaf@hotmail.co.th>
2022-09-02 10:08:34 +0700

    ex 3.2 Ver 1

---
+++ src/serverwofacade/FacadeServer.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverwofacade;
+
+/**
+ *
+ * @author Tinip
+ */
+public class FacadeServer {
+    static ScheduleServer scheduleServer = new ScheduleServer();
+    public static void StartServer(){
+	scheduleServer.startBooting();
+	scheduleServer.readSystemConfigFile();
+	scheduleServer.init();
+	scheduleServer.initializeContext();
+	scheduleServer.initializeListeners();
+	scheduleServer.createSystemObjects();
+	System.out.println("Start working......");
+    }
+    
+    public static void StopServer(){
+        System.out.println("After work done.........");
+	scheduleServer.releaseProcesses();
+	scheduleServer.destory();
+	scheduleServer.destroySystemObjects();
+	scheduleServer.destoryListeners();
+	scheduleServer.destoryContext();
+	scheduleServer.shutdown();
+    }
+}
